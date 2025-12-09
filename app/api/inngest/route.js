056065d097df9dcaddf4ee0b200a2f93ed14ae79
srinjoy2075 <iamsrinjoy03@gmail.com>
2025-12-09 19:03:52 +0530

    New files

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,12 @@
+import { inngest } from "@/lib/inngest/client";
+import { helloWorld } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
-import { inngest } from "../../inngest/client";
-import { helloWorld } from "../../inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
-  client: InngestApi,
+  client: inngest,
   functions: [
+    helloWorld
     /* your functions will be passed here later! */
   ],
 });
--- components/header.jsx
+++ components/header.jsx
@@ -5,8 +5,10 @@ import React from 'react'
 import { Button } from './ui/button'
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { checkUser } from '@/lib/checkUser'
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
