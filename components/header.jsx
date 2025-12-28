9bde15d887ee9ac90d521805450041c450e9c2ea
srinjoy2075 <iamsrinjoy03@gmail.com>
2025-12-28 20:55:14 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -14,11 +14,11 @@ const Header = async () => {
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo.png"}
-            alt="Sensai Logo"
-            width={200}
-            height={60}
-            className="h-12 py-1 w-auto object-contain"
+            src={"/ascent.png"}
+            alt="Ascent Logo"
+            width={320}
+            height={300}
+            className="h-20 py-3 w-auto object-contain"
           />
         </Link>
 
